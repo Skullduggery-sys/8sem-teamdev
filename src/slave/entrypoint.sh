a6674db0c715166a24ec9fde36a10759eb4dcc01
perpetualgod <perpetualgod@yandex.ru>
2025-04-06 13:55:56 +0300

    [dev]: get rid of node_modules & master-slave replica files

--- src/slave/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Change ownership of the data directory
-chown -R 999:999 /var/lib/postgresql/data-replics
-
-# Run the original PostgreSQL entrypoint
-exec /usr/local/bin/docker-entrypoint.sh "$@"
